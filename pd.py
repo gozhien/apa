42457afb6e3ba0b6f1e31a4a4e220e742e847754
gozhien <91651757+gozhien@users.noreply.github.com>
2021-12-21 11:35:25 +0700

    Update pd.py

--- pd.py
+++ pd.py
@@ -13,7 +13,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:1194'
-RESPONSE = 'HTTP/1.1 101 <font color="purple">FAS TUNNEL</font>\r\nContent-length: 1048576000000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 <font color="green">FAS TUNNEL</font>\r\nContent-length: 1048576000000\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
